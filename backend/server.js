e2b309cb1eb0338b36fcaf7ddd5ac8884a984277
gk12 <gk44035@gmail.com>
2022-08-04 12:14:19 +0530

    product route is working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const app=require("./app");
 
 const dotenv =require("dotenv");
+
+
+//config
+dotenv.config({path:"backend/config/config.env"});
+
+
 //create a server
 const server=app.listen(process.env.PORT,()=>
 {
