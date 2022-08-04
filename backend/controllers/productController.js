b623aa33e1004afc9c38e2123e861f133b9da3bd
gk12 <gk44035@gmail.com>
2022-08-04 13:51:58 +0530

    update and delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,3 +18,49 @@ exports.getAllProducts=async(req,res)=>{
         products
     })
 }
+
+// Update Product -- Admin
+exports.updateProduct=async(req,res,next)=>{
+    let product=await Product.findById(req.param.id);
+
+
+    if(!product)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product=await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        userFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    });
+}
+
+//DELETE PRODUCT
+
+exports.deleteProduct=async(req,res,next)=>{
+    const product=await Product.findById(req.params.id);
+
+    if(!product)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+  await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"Product deleted successfully"
+    });
+
+}
