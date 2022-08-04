2a7b9ce3fb40972fced3f0dc17c968970bda2f1a
gk12 <gk44035@gmail.com>
2022-08-04 13:14:50 +0530

    schema for product

---
+++ backend/models/productModel.js
@@ -0,0 +1,91 @@
+const mongoose=require("mongoose");
+const productSchema= new mongoose.Schema({
+// name of product
+    name:{
+        type:String,
+        required:[true,"Please enter product name"],
+        trim:true
+    },
+
+//description of product
+    description:{
+        type:String,
+        required:[true,"Please Enter product Description"]
+    },
+
+//price of product
+    price:{
+        type:Number,
+    required:[true,"Please Enter product price"],
+    maxLength:[8,"Price cannot exceed 8 char"]
+   },
+
+//product rating
+   rating:{
+    type:Number,
+    default:0
+   },
+
+//product image
+   images:
+   [
+    {
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+       }
+   ],
+
+//product category (phone,loptop etc)
+   category:{
+    type:String,
+    required:[true,"Please Enter product Category"],
+   },
+
+//product in stock
+   stock:{
+    type:Number,
+    required:[true,"Please enter product Stock"],
+    maxLength:[4,"Stock cannot exceed 4 characters"],
+    default:1
+   },
+
+//total number of reviews of product
+   numOfReviews:{
+    type:Number,
+    default:0
+   },
+
+// product reviews
+   reviews:[
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        rating:{
+            type:Number,
+            required:true
+        },
+        comment:{
+            type:String,
+            required:true
+        }
+
+    }
+   ],
+
+//date of creation of product
+   createdAt:{
+    type:Date,
+    default:Date.now
+   }
+
+})
+
+module.exports=mongoose.model("Product",productSchema);
