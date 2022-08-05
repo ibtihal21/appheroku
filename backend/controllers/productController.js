923e1cfa1273a5077c3e8fe2bfa97752c26fba34
gk12 <gk44035@gmail.com>
2022-08-05 16:40:10 +0530

    apifeatures_filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,8 @@ exports.createProduct=catchAsyncErrors(async(req,res,next)=>{
 //Get all products
 exports.getAllProducts=catchAsyncErrors(async(req,res)=>{
     const apiFeature=new ApiFeatures(Product.find(),req.query)
-    .search();
+    .search()
+    .filter();
     const products=await apiFeature.query;
     res.status(200).json({
         success:true,
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -19,6 +19,26 @@ search(){
     this.query=this.query.find({...keyword});
     return this;
 }
+
+// this is for category
+filter(){
+    const queryCopy={...this.queryStr};
+
+    //removing some fields for category
+
+    const removedFields=["keyword","page","limit"];
+
+    removedFields.forEach((key)=> delete queryCopy[key]);
+
+    //filter price and rating
+    // console.log(queryCopy);
+    let queryStr=JSON.stringify(queryCopy);
+     queryStr=queryStr.replace(/\b(gt|gte|lt|lte)\b/g,(key)=>`$${key}`);
+
+    this.query=this.query.find(JSON.parse(queryStr));
+    return this;
+}
+
 }
 
 module.exports=ApiFeatures;
