9f939a4a2c09ec23267745f6374cc0c159f69c71
gk12 <gk44035@gmail.com>
2022-08-05 13:53:49 +0530

    mongodb_error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports=(err,req,res,next)=>{
     err.statusCode=err.statusCode || 500;
     err.message=err.message || "Internal server Error";
 
+     //wrong mongodb id error
+     if(err.name==="CastError")
+     {
+         const message=`Resource not found. Invalid:${err.path}`;
+         err=new ErrorHander(message,400);
+     }
 
     res.status(err.statusCode).json({
         success:false,
