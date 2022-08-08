185fd62b802ab917cd589f4d60921e94cac1e25e
gk12 <gk44035@gmail.com>
2022-08-08 18:54:12 +0530

    usercontroller_loggedOut

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,21 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("./catchAsyncErrors");
 
 const isAuthenticatedUser=catchAsyncErrors(async(req,res,next)=>
 {
-    const token=req.cookies;
-    console.log(token);
+    const {token}=req.cookies;
+    
+    if(!token)
+    {
+        return next(new ErrorHander("Please Login to access this resource",401));
+    }
+
+    const decodedData=jwt.verify(token,process.env.JWT_SECRET);
+
+    req.user=await User.findById(decodedData.id);
+    next();
 });
 
 module.exports=isAuthenticatedUser;
