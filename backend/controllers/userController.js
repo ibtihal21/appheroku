6d3c1361017014dca4143c081f7d4dc9e9678ebc
gk12 <gk44035@gmail.com>
2022-08-14 00:43:33 +0530

    route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -220,3 +220,44 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
         user,
     });
   });
+
+  //update user Role--Admin
+  exports.updateUserRole=catchAsyncErrors(async(req,res,next)=>{
+    const newUserData={
+        name:req.body.name,
+        email:req.body.email,
+        role:req.body.role,
+    };
+
+    const user=await User.findByIdAndUpdate(req.params.id,newUserData,{
+        new:true,
+        runValidators:true,
+        userFindAndModify:false,
+    });
+
+    res.status(200).json({
+        success:true,
+    });
+  });
+
+  //Delete user Role--Admin
+  exports.deleteUser=catchAsyncErrors(async(req,res,next)=>{
+    
+    const user=await User.findByIdAndUpdate(req.params.id)
+    //we  will remove cloudinary later
+
+    if(!user){
+        return next(
+            new ErrorHander(`user does not exist with id:${req.params.id}`,400)
+        );
+    }
+
+    await user.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"User deleted successfully",
+    });
+  });
+
+  
