cb936c3a7b84a0dd0bef544d3a64f5b8ad0db017
gk12 <gk44035@gmail.com>
2022-08-13 17:21:36 +0530

    admin_route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -9,12 +9,13 @@ router
 .get(getAllProducts);
 
 router
-.route("/product/new")
+.route("/admin/product/new")
 .post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
 
 router
-.route("/product/:id")
+.route("/admin/product/:id")
 .put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct)
 .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
-.get(getProductDetails);
+
+router.route("/product/:id").get(getProductDetails);
 module.exports=router
