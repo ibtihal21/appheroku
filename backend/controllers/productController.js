beddb95b64c14959a4668e1754240eb3462a84f9
gk12 <gk44035@gmail.com>
2022-08-14 01:10:41 +0530

    review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -125,4 +125,10 @@ exports.createProductReview=catchAsyncErrors(async(req,res,next)=>
         avg+=rev.rating;
 
     })/product.reviews.length;
+
+    await product.save({validateBeforeSave:false});
+
+    res.status(200).json({
+        success:true,
+    })
 });
