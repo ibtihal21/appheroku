0b7e227c577302d451e5d08b0146ebbdbcaf52ce
gk12 <gk44035@gmail.com>
2022-08-14 18:02:58 +0530

    get_and_delete_review_route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview } = require("../controllers/productController");
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
 const {isAuthenticatedUser,authorizeRoles} = require("../middleware/auth");
 
 const router=express.Router();
@@ -19,4 +19,8 @@ router
 
 router.route("/product/:id").get(getProductDetails);
 router.route("/review").put(isAuthenticatedUser,createProductReview);
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
 module.exports=router
+
+//isAuthenticatedUser ka mtlab hai wo sirf admin login jo hoga wo karega
+//getreview me nahi hai mtlab koi bhi review ko dekh skta hai
