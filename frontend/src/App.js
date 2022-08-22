985d06d2bf864aa3659fd16a42abca8eb10c2a5e
gk12 <gk44035@gmail.com>
2022-08-22 23:43:56 +0530

    header.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import Header from "./component/layout/Header.js"
-
 import {BrowserRouter as Router} from "react-router-dom"
+import WebFont from "webfontloader";
+import React from 'react';
 function App() {
+
+  React.useEffect(()=>{
+    WebFont.load({
+      google:{
+        families:['Roboto','Droid Sans'],
+      },
+    });
+  },[]);
+
   return <Router>
     <Header/>
   </Router>;
