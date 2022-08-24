769862804cda3e074cf405b38076d61883be5c68
gk12 <gk44035@gmail.com>
2022-08-24 08:47:03 +0530

    hOME_JS

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from 'react'
 import {CgMouse} from "react-icons/cg";
 import "./Home.css"
+import Product from "./Product.js";
+
+const product={
+  name:"White Shirt",
+  image:[{url:"shorturl.at/AGKY8"}],
+  price:"$20",
+  _id:"nothing",
+};
 const Home = () => {
   return (
     <Fragment>
@@ -13,6 +21,12 @@ const Home = () => {
                     Scroll <CgMouse/>
                 </button>
             </a>
+        </div>
+
+        <h2 className='homeHeading'>Featured Products</h2>
+
+        <div className='container ' id="container">
+          <Product product={product}/>
         </div>
     </Fragment>
   );
