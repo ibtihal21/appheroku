e0fea64fb3fcff8dcb472cf09a28751d8fbb5daa
gk12 <gk44035@gmail.com>
2022-09-02 20:59:15 +0530

    PRODUCT_DETAILS_REDUCER

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -2,6 +2,9 @@ import{
     ALL_PRODUCT_FAIL,
     ALL_PRODUCT_REQUEST,
     ALL_PRODUCT_SUCCESS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_SUCCESS,
     CLEAR_ERRORS,
 }from "../constants/productConstants";
 
@@ -33,4 +36,36 @@ action)=>{
                     default:
                         return state;
     }
-}
+};
+
+
+export const productDetailsReducer=
+(state={products:[]},
+action)=>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return{
+                loading:true,
+                ...state,
+            };
+            case PRODUCT_DETAILS_SUCCESS:
+                return{
+                    loading:false,
+                    products:action.payload,
+                };
+                case PRODUCT_DETAILS_FAIL:
+                    return{
+                        loading:false,
+                        error:action.payload,
+                    };
+                    case CLEAR_ERRORS:
+                        return{
+                            ...state,
+                            error:null,
+                        };
+                    default:
+                        return state;
+    }
+};
+
+
