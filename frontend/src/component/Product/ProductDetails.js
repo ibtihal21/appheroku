cb46e91414f8538654fd9c61ab55fe3eef577a8e
gk12 <gk44035@gmail.com>
2022-09-03 12:47:43 +0530

    Product_details.js

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,8 +1,27 @@
 import React ,{Fragment} from 'react'
 import  Carousel  from "react-material-ui-carousel";
+import Product from '../Home/Product';
 import "./ProductDetails.css";
 const ProductDetails = () => {
-  return <div>ksbdjkskfbksbfjksj</div>;
+  return (
+    <Fragment>
+        <div className='ProductDetails'>
+            <div>
+                <Carousel>
+                    {product.images && product.images.map((item,i)=>
+                     (<img
+                      className='CaroselImage'
+                      key={item.url}
+                      src={item.url}
+                      alt={`${i}Slide`}
+                      />
+                    ))}
+                </Carousel>
+            </div>
+
+        </div>
+    </Fragment>
+  )
   
 }
 
