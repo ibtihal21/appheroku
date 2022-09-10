a712785a796e779e922b46e7229866d4b22279e4
gk12 <gk44035@gmail.com>
2022-09-10 23:20:20 +0530

    HOME_JS_UPDATED

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,61 +1,55 @@
-import React, { Fragment, useEffect } from 'react'
-import {CgMouse} from "react-icons/cg";
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/all";
 import "./Home.css";
-import Product from "./Product.js";
-import Metadata from '../layout/Metadata';
-import { getProduct } from '../../actions/productAction';
-import {useSelector,useDispatch} from "react-redux";
-import Loader from '../layout/Loader/Loader';
-import { useAlert } from 'react-alert';
-//product details export to product.js
-//product ka object bnaya tha ab eska koi kam nahi hai
-//pahle 8 products esi se bnaya tha
-
+import ProductCard from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 const Home = () => {
-  const alert=useAlert();
-  const dispatch=useDispatch();
-  const {loading,error,products}=useSelector(
-    (state)=>state.products
-  );
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
 
-  useEffect(()=>{
-    if(error)
-    {
-      return alert.error(error);
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
     dispatch(getProduct());
-  },[dispatch,error,alert]);
-  
+  }, [dispatch, error, alert]);
+
   return (
-  
     <Fragment>
-      {loading?(
-      <Loader/>
-      ):(
-          <Fragment>
-          {/* PROJECT KA TITLE UPER DIKHEGA */}
-          <Metadata title="SUPERSHOP"/> 
-            <div className="banner">
-                <p>WELCOME TO SUPERSHOP</p>
-                <h1>Find Amazing Products</h1>
-    
-                <a href="#container">
-                    <button>
-                        Scroll <CgMouse/>
-                    </button>
-                </a>
-            </div>
-    
-            <h2 className="homeHeading">Featured Products</h2>
-    
-            <div className="container " id="container">
-              {/* products ko map use ker ke kam ker rahe hai pahle alag alag likha tha */}
-              {
-              products && products.map((product)=><Product key={product._id} product={product}/>
-              )}
-            </div>
-        </Fragment>)}
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="SUPERSHOP" />
+
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="homeHeading">Featured Products</h2>
+
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
