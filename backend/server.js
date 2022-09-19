e804f75fa711c00963e049b6fb6e6afd8f07ed86
gk12 <gk44035@gmail.com>
2022-09-19 23:53:52 +0530

    update about

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ process.on("uncaughtException",err=>{
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
 
-dotenv.config({path:"backend/config/config.env"});
+require("dotenv").config({path:"backend/config/config.env"});
 }
 
 //connecting to databse
