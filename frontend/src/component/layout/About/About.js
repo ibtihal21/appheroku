65fbbab07213c7fbac0f6d984da58074a4f3502a
gk12 <gk44035@gmail.com>
2022-09-26 22:16:55 +0530

    update

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -3,9 +3,9 @@ import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedinIcon from "@material-ui/icons/LinkedIn";
 const About = () => {
   const visitInstagram = () => {
-    //put instagram link of supershop
     window.location = "https://www.instagram.com/__gauravjha__/";
   };
   return (
@@ -16,35 +16,56 @@ const About = () => {
         <Typography component="h1">About Us</Typography>
 
         <div>
+
+
           <div>
-            <Avatar
-              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+          <span>
+              This is an E-commerece website(Supershop) made by using MERN stack where user can buy and checkout product.
+              And add review and rating for the product. 
+            </span>
+          <Avatar
+              style={{ width: "20vmax", height: "20vmax", margin: "2vmax 0" }}
               //put founder image at abc.png
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/abc.png"
+              src="https://res.cloudinary.com/gk35044/image/upload/v1664183793/avatars/logo_qqqtid.png"
+              alt="Website Logo"
+            />
+          </div>
+
+          <div>
+          <h1>Designed and Developed By</h1>
+
+            <Avatar
+              style={{ width: "15vmax", height: "15vmax", margin: "2vmax 0" }}
+              //put founder image at abc.png
+              src="https://res.cloudinary.com/gk35044/image/upload/v1664181456/avatars/WhatsApp_Image_2022-09-26_at_2.07.00_PM_ubggul.jpg"
               alt="Founder"
             />
-            <Typography>Gaurav Kumar</Typography>
-            <Button onClick={visitInstagram} color="primary">
+            <h2>Gaurav Kumar</h2>
+            {/* <Button onClick={visitInstagram} color="primary">
               Visit Instagram
-            </Button>
-            <span>
-              This is a sample wesbite made by me(Gaurav Kumar).
-            </span>
+            </Button> */}
+          
           </div>
-          <div className="aboutSectionContainer2">
+          {/* yaha maine pahle utbue,insta,linkedin ka logo and link daala tha */}
+          {/* <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
-            //put utube link of supershop
+            put utube link of supershop
               href="https://www.youtube.com/channel/UCbpFB6_KJJPYKdgAwPgqJww"
               target="blank"
             >
               <YouTubeIcon className="youtubeSvgIcon" />
             </a>
-               {/* put insta link */} 
+               put insta link 
              <a href="https://www.instagram.com/__gauravjha__/" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
-            </a>
-          </div>
+              </a>
+
+              <a href="https://www.linkedin.com/in/gauravkumar44/" target="blank">
+              <LinkedinIcon className="linkedinSvgIcon"/>
+
+              </a>
+          </div> */}
         </div>
       </div>
     </div>
--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./Contact.css";
-import { Button } from "@material-ui/core";
+import {Typography} from "@material-ui/core";
+import lmail from "@material-ui/icons/Email";
+import lphone from "@material-ui/icons/ContactPhone";
 
 const Contact = () => {
   return (
+   
+    <div className="contact">
+    <div></div>
+    <div className="contactGradient"></div>
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:gridharkumar279@gmail.com">
-        <Button ><span>Contact : Mail Id :-gridharkumar279@gmail.com</span></Button>
-        
-
-      </a>
+      <Typography component="h1">Contact Us</Typography>
+     <div>
+      <a
+          href="mailto:gridharkumar279@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          >
+          gridharkumar279@gmail.com
+        </a>
+      </div>
     </div>
+    </div>
+    
   );
 };
 
